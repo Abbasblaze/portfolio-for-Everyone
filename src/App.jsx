1670ee50f4caf5044bebf0c7b77988cd95fc83e5
Abbasblaze <abbas.raazaa40@gmail.com>
2024-09-18 13:19:45 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import './App.css'
 import AboutMe from './Components/AboutMe/AboutMe'
-// import Certifications from './Components/Certifications/Certifications'
-// import Contact from './Components/Contact/Contact'
-// import Experience from './Components/Experience/Experience'
+import Certifications from './Components/Certifications/Certifications'
+import Contact from './Components/Contact/Contact'
+import Experience from './Components/Experience/Experience'
 import Header from './Components/Header/Header'
 import MyProjects from './Components/MyProjects/MyProjects'
 import Navigation from './Components/Navigation/Navigation'
@@ -19,9 +19,9 @@ function App() {
      <AboutMe/>
      <MyProjects/>
     <Skills/>
-     {/*  <Certifications/>
-     <Experience/> */}
-     {/* <Contact/> */}
+      <Certifications/>
+     <Experience/>
+     <Contact/>
     </>
   )
 }
