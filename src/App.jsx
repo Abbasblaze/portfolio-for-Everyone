943a1ab93d59e5b686de0535f1f39e4c13826231
Abbasblaze <abbas.raazaa40@gmail.com>
2024-09-18 13:38:46 +0500

    changes small screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
 import AboutMe from './Components/AboutMe/AboutMe'
-import Certifications from './Components/Certifications/Certifications'
-import Contact from './Components/Contact/Contact'
-import Experience from './Components/Experience/Experience'
+// import Certifications from './Components/Certifications/Certifications'
+// import Contact from './Components/Contact/Contact'
+// import Experience from './Components/Experience/Experience'
 import Header from './Components/Header/Header'
-import MyProjects from './Components/MyProjects/MyProjects'
+// import MyProjects from './Components/MyProjects/MyProjects'
 import Navigation from './Components/Navigation/Navigation'
-import Skills from './Components/Skills/Skills'
+// import Skills from './Components/Skills/Skills'
 
 function App() {
 
@@ -17,11 +17,11 @@ function App() {
      <Navigation/>
      <Header/>
      <AboutMe/>
-     <MyProjects/>
-    <Skills/>
-      <Certifications/>
-     <Experience/>
-     <Contact/>
+     {/* <MyProjects/> */}
+    {/* <Skills/> */}
+      {/* <Certifications/> */}
+     {/* <Experience/> */}
+     {/* <Contact/> */}
     </>
   )
 }
